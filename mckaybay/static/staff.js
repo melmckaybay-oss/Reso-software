b0f2fc78793a1dc5cc41183920c19b9f9c426b98
melmckaybay-oss <melmckaybay@gmail.com>
2026-04-25 15:08:16 -0700

    Add files via upload

--- mckaybay/static/staff.js
+++ mckaybay/static/staff.js
@@ -562,6 +562,11 @@ const Staff = (() => {
     @media print {
       body { margin: 10px; }
       button { display: none; }
+      * {
+        -webkit-print-color-adjust: exact !important;
+        print-color-adjust: exact !important;
+        color-adjust: exact !important;
+      }
     }
   </style>
 </head><body>
